86714a95468b38c944daee00c4419d059de09304
maven <devmaveen333@yahoo.com>
2024-08-11 10:37:59 -0700

    add loading skeletons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import { Table } from '@radix-ui/themes'
-import prisma from '@/prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBadge'
+import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import IssueActions from './IssueActionButtons'
 
 
-const IssuesPage = async () => {
-    //fetch all uses in the database
-    const issues = await prisma.issue.findMany();
+const loadingIssuePage = () => {
+
+    const issues = [1, 2, 3, 4, 5]
+
     return (
         <div>
             <IssueActions />
@@ -21,19 +22,19 @@ const IssuesPage = async () => {
                 </Table.Header>
                 <Table.Body>
                     {issues.map(issue => (
-                        <Table.Row key={issue.id}>
+                        <Table.Row key={issue}>
                             <Table.Cell>
-                                {issue.title}
-                                <div className='block md:hidden'><IssueStatusBadge status={issue.status} /></div>
+                                <Skeleton />
+                                <div className='block md:hidden'><Skeleton /></div>
                             </Table.Cell>
-                            <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
-                            <Table.Cell className='hidden sm:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+                            <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
+                            <Table.Cell className='hidden sm:table-cell'><Skeleton /></Table.Cell>
                         </Table.Row>
-                    ))}
+                    ))};
                 </Table.Body>
             </Table.Root>
         </div>
     )
 }
 
-export default IssuesPage
+export default loadingIssuePage
