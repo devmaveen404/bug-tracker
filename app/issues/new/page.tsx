527271a29f680d3e34b08c1aac9ee3a93a085fe2
maven <devmaveen333@yahoo.com>
2024-08-12 16:58:48 +0100

    Build edit issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,80 +1,10 @@
-'use client'
-import { Button, Callout, TextField, Text } from '@radix-ui/themes'
-import "easymde/dist/easymde.min.css";
-// handling form submission
-import { useForm, Controller } from 'react-hook-form'
-import axios from 'axios';// submit data to database
-import React, { useState } from 'react'
-import { useRouter } from 'next/navigation';
-import { IoInformationCircleOutline } from "react-icons/io5";
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod'
-import ErrorMessage from '@/app/components/ErrorMessage';
-import { Spinner } from '@radix-ui/themes';
-import dynamic from 'next/dynamic';
+import React from 'react'
+import IssueForm from '../_components/IssueForm'
 
-// since markdown editor(client component) is rendered on server, markdown should be lazy loaded
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'),
-    { ssr: false }
-);
-
-// shape of the issue form
-// interface IssueForm {
-//     title: string
-//     description: string
-// }
-// shape of issue form based on schema
-type IssueForm = z.infer<typeof createIssueSchema>
-
-const NewIssuePage = () => {
-
-    //redirect user to issue page
-    const router = useRouter()
-    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
-        //zod integration with hookform
-        resolver: zodResolver(createIssueSchema)
-    });
-    //Handle Error when empty fiel is sumbitted
-    const [error, setError] = useState('')
-    const [isSubmitting, setIsSubmitting] = useState(false);
-
-    return (
-        <div className='max-w-xl'>
-            {error && <Callout.Root color='red' className='mb-5' >
-                <Callout.Icon>
-                    <IoInformationCircleOutline />
-                </Callout.Icon>
-                <Callout.Text>
-                    {error}
-                </Callout.Text>
-            </Callout.Root>}
-            <form
-                className='space-y-3'
-                onSubmit={handleSubmit(async (data) => {
-                    try {
-                        setIsSubmitting(true)
-                        await axios.post('/api/issues', data)
-                        router.push('/issues')
-                    } catch (error) {
-                        setIsSubmitting(false)
-                        setError('Input details properly.')
-                    }
-                })}>
-                <TextField.Root placeholder='Title...' {...register('title')} />
-                {errors.title && <ErrorMessage>{errors.title.message}</ErrorMessage>}
-                <Controller
-                    name='description'
-                    control={control}
-                    render={({ field }) => <SimpleMDE placeholder="Description..." {...field} />}
-                />
-                {errors.description && <ErrorMessage>{errors.description.message}</ErrorMessage>}
-                <Button disabled={isSubmitting}>
-                    Submit New Issue {isSubmitting && <Spinner />}
-                </Button>
-            </form>
-        </div>
-    )
+const page = () => {
+  return (
+    <IssueForm />
+  )
 }
 
-export default NewIssuePage
+export default page
