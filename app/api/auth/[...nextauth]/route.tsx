b714d0f591554f2dc0af07418b888f2648e1761f
maven <devmaveen333@yahoo.com>
2024-08-14 17:05:30 +0100

    configure google provider

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,8 +1,14 @@
 //a catch all segment to catch all auth related pages i.e signin, signout etc
 import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    providers: []
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
+    ]
 })
 
 export { handler as GET, handler as POST }
