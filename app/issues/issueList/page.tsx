02af6ace112a1ab330cffe212fe9f931ee5283eb
maven <devmaveen333@yahoo.com>
2024-08-20 16:23:09 +0100

    implement descending sort

--- app/issues/issueList/page.tsx
+++ app/issues/issueList/page.tsx
@@ -4,13 +4,17 @@ import { Issue, PrismaClient, Status } from '@prisma/client'
 import { IssueStatusBadge } from '@/app/components'
 import IssueActions from './IssueActionButtons'
 import Link from 'next/link'
-import { IoArrowUp } from 'react-icons/io5'
+import { IoArrowUp, IoArrowDown } from 'react-icons/io5'
 
 
 const prisma = new PrismaClient()
 
 interface Props {
-    searchParams: { status: Status, orderBy: keyof Issue };
+    searchParams: {
+        status: Status,
+        orderBy: keyof Issue
+        sort: 'asc' | 'desc'
+    };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
@@ -28,15 +32,23 @@ const IssuesPage = async ({ searchParams }: Props) => {
     // validate issues, issues status, before rendering
     //returns status properties
     const statuses = Object.values(Status)
-    // a)
+    // a) filter issues by status
     const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
 
-    //fetch all uses in the database
+    //sort issues in asc
+    const orderBy = columns.map(columns => columns.value).includes(searchParams.orderBy) ? { [searchParams.orderBy]: searchParams.sort } : undefined;
+
+    const toggleOrder = () => {
+        return !searchParams.sort || searchParams.sort === "desc" ? "asc" : "desc";
+    };
+
+    //fetch all issues in the database
     const issues = await prisma.issue.findMany({
         where: {
             // b)
             status
-        }
+        },
+        orderBy
     });
     return (
         <div className='max-w-xl'>
@@ -45,15 +57,16 @@ const IssuesPage = async ({ searchParams }: Props) => {
                 <Table.Header>
                     <Table.Row>
                         {columns.map((column) => (
-                            <Table.ColumnHeaderCell key={column.value}>
+                            <Table.ColumnHeaderCell key={column.value} className={column.className}>
                                 <Link href={{
                                     query: {
-                                        ...searchParams, orderBy: column.value
+                                        ...searchParams, orderBy: column.value, sort: toggleOrder()
                                     }
-                                }}>
+                                }}
+                                >
                                     {column.label}
                                 </Link>
-                                {column.value === searchParams.orderBy && <IoArrowUp className='inline' />}
+                                {column.value === searchParams.orderBy && (searchParams.sort === 'asc' ? <IoArrowUp className='inline' /> : <IoArrowDown className='inline' />)}
                             </Table.ColumnHeaderCell>
                         ))}
                     </Table.Row>
