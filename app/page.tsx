c6a4cc4245ee4a1ff79ceca3fecb6a2ce318a3a3
maven <devmaveen333@yahoo.com>
2024-08-21 23:34:11 +0100

    build issue summary page

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,27 @@ import { authOptions } from './api/auth/[...nextauth]/route'
 import { Em, Heading } from '@radix-ui/themes';
 import Pagination from './components/pagination';
 import LatestIssues from './LatestIssues';
+import IssueSummary from './IssueSummary';
+import { PrismaClient } from '@prisma/client';
 
-export default async function Home({ searchParams }: { searchParams: { page: string }; }) {
+
+const prisma = new PrismaClient()
+
+export default async function Home() {
 
   //access session on server
   const session = await getServerSession(authOptions);
 
+  // fetch issue status from database\
+  const open = await prisma.issue.count({ where: { status: 'OPEN' } })
+  const inProgress = await prisma.issue.count({ where: { status: 'IN_PROGRESS' } })
+  const closed = await prisma.issue.count({ where: { status: 'CLOSED' } })
+
   return (
     <>
       <Heading mb={'8'}>Welcome{session && <Em>{`, ${session.user!.name}`}</Em>}</Heading>
-      <LatestIssues />
+      {/* <LatestIssues /> */}
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
     </>
   )
 }
